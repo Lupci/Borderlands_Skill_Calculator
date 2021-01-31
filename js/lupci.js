bd7d95909c692874a69340110172a0d2920a7c72
Lupci <69643444+Lupci@users.noreply.github.com>
2021-01-30 23:49:35 -0300

    testing mobile

--- js/lupci.js
+++ js/lupci.js
@@ -61,6 +61,18 @@ function handleMouseup(event) {
 	}
 }
 
+/* Lupci trying to add mobile support */
+$(document).on("div.skill",function() {
+            $("span.points").on("tap",function() {
+               $(this).updatePoints(lastTouched, -1);
+               $(this).updatePoints(lastTouched, -1);
+               $(this).updatePoints(lastTouched, -1);
+               $(this).updatePoints(lastTouched, -1);
+               $(this).updatePoints(lastTouched, -1);
+            });
+         });
+/* does it work? */
+
 function checkLongTouch(fromTimer) {
 	if (lastTouched !== null) {
 		if (fromTimer === true) {
